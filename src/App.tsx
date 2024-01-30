eb0ee91c579398ec4152c92151813d45d8b813c9
Andrey Podovinov <andrey.podovinov@iqtek.ru>
2024-01-30 15:53:17 +0600

    added Services component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import classes from "./App.module.css";
 import Header from "./components/Header";
+import Services from "./components/Services";
 
 function App() {
     return (
         <>
             <Header></Header>
+            <main>
+                <Services />
+            </main>
         </>
     );
 }
---
+++ src/components/Services/Services.tsx
@@ -0,0 +1,52 @@
+import classes from "./Services.module.css";
+import { strings } from "../../resources/strings";
+import Delimiter from "../Delimiter";
+import ServiceCard from "../ServiceCard";
+import Button from "../Button";
+
+function Services() {
+    return (
+        <div className={classes.services}>
+            <div className={classes.servicesInfo}>
+                <h2>{strings.services_block_title}</h2>
+                <Delimiter className={classes.delimiter} />
+                <p>{strings.services_block_description}</p>
+            </div>
+            <div className={classes.serviceCards}>
+                <ServiceCard
+                    image={strings.service1_image}
+                    title={strings.service1_title}
+                    description={strings.service1_description}
+                />
+                <ServiceCard
+                    image={strings.service2_image}
+                    title={strings.service2_title}
+                    description={strings.service2_description}
+                />
+                <ServiceCard
+                    image={strings.service3_image}
+                    title={strings.service3_title}
+                    description={strings.service3_description}
+                />
+                <ServiceCard
+                    image={strings.service4_image}
+                    title={strings.service4_title}
+                    description={strings.service4_description}
+                />
+                <ServiceCard
+                    image={strings.service5_image}
+                    title={strings.service5_title}
+                    description={strings.service5_description}
+                />
+                <ServiceCard
+                    image={strings.service6_image}
+                    title={strings.service6_title}
+                    description={strings.service6_description}
+                />
+            </div>
+            <Button text={strings.services_block_button_text} fill={false} className={classes.button} />
+        </div>
+    );
+}
+
+export default Services;
